49b933d6605bf1b10660717b121e1af0849f3f7e
Anthony Xavier <anthony-xavier@live.com>
2022-01-10 15:46:37 +1000

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "inline-source-map",
+
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    clean: true
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    compress: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "CRWN - Clothing",
+      template: "public/index.html",
+      filename: "index.html"
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      }
+    ]
+  }
+};
